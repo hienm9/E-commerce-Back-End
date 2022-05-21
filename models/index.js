08b2b8127a4948f6d56e5d0b2a036ff33b27e3d5
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-05-21 00:39:31 -0400

    update index models, models complete git push

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: {name: 'category_id', unique: false},
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: {name: 'product_id', unique: false},
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: {name: 'tag_id', unique: false},
+});
 
 module.exports = {
   Product,
